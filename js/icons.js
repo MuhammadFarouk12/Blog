74b66ed92d4bcb9c4cf349f291350435e33f0dcf
Muhammad Farouk <muhammad12fk@gmail.com>
2021-11-15 13:56:48 +0300

    Last Touches And Handling Errors

--- js/icons.js
+++ js/icons.js
@@ -1,6 +1,8 @@
-let socialIcons = Array.from(document.querySelectorAll(".container.lastArtcicles .bussenissSide .social .icons i"));
-document.querySelector('.first').onclick = _=>{
-    socialIcons.forEach(e=>{
-        e.classList.toggle('active');
-    })
-}
+setTimeout(function() {
+    let socialIcons = Array.from(document.querySelectorAll(".container.lastArtcicles .bussenissSide .social .icons i"));
+    document.querySelector('.first').onclick = _=>{
+        socialIcons.forEach(e=>{
+            e.classList.toggle('active');
+        })
+    }
+},1000)
--- js/main-nav.js
+++ js/main-nav.js
@@ -1,7 +1,9 @@
-mainSearch.onmouseover = ()=>mainSearch.firstElementChild.classList.add('active');
-mainSearch.onmouseout = ()=>mainSearch.firstElementChild.classList.remove('active');
-burger.onclick = ()=>{
-    options.classList.toggle('active');
-    burger.classList.toggle('active');
-};
-changeTheme.onclick = ()=>document.firstElementChild.classList.toggle('dark');
+setTimeout(function() {
+    mainSearch.onmouseover = ()=>mainSearch.firstElementChild.classList.add('active');
+    mainSearch.onmouseout = ()=>mainSearch.firstElementChild.classList.remove('active');
+    burger.onclick = ()=>{
+        options.classList.toggle('active');
+        burger.classList.toggle('active');
+    };
+    changeTheme.onclick = ()=>document.firstElementChild.classList.toggle('dark');
+},1000)
